acef24b9ea9edaae81df5f28a3d9cf2da392a2cb
anet-eny <anet.eny@gmail.com>
2024-12-11 20:30:02 +0100

    Refactor generateAllNewDice() which returns obj with isHeld and id properties, add nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Die from "./Die"
+import { nanoid } from "nanoid"
 
 
 
@@ -15,10 +16,14 @@ export default function App() {
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
-      .map(() => Math.ceil(Math.random() * 6))
+      .map(() => ({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      }))
   }
 
-  const diceElements = dice.map(number => <Die value={number}/>)
+  const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value}/>)
 
   
   return (
