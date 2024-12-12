f1b41b9d26c062f3334e03581ac918106ce0f8c0
anet-eny <anet.eny@gmail.com>
2024-12-12 20:43:39 +0100

    Create gameWon var and change the btn text based on gameWon statement

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ export default function App() {
 
   const [dice, setDice] = useState(generateAllNewDice())
 
+  const gameWon = dice.every(die => die.isHeld) && 
+     dice.every(die => die.value === dice[0].value)
+  
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
@@ -44,7 +47,7 @@ export default function App() {
       <div className="dice-container">
       {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>{gameWon ? "New game" : "Roll"}</button>
     </main>
   )
 }
