16a019aac2eeca302e12fd3077948388c2f78182
anet-eny <anet.eny@gmail.com>
2024-12-12 21:09:39 +0100

    Add ConfettiComponent and use Confetti when the game is won

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import ConfettiComponent from "./assets/ConfettiComponent"
 
 
 
@@ -42,6 +43,7 @@ export default function App() {
   
   return (
     <main>
+      {gameWon && <ConfettiComponent />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
