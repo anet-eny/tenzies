95e1b6e64a125472b6fcad6968427e5a7ae076da
anet-eny <anet.eny@gmail.com>
2024-12-12 19:51:12 +0100

    Refactor rollDice() to change values of not held dice

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
   }
 
   function rollDice() {
-    setDice(generateAllNewDice())
+    setDice(prevDice => prevDice.map(die => die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}))
   }
 
   function hold(id) {
